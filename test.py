bb702113c26353eef7152e2bb7eda1db4704b1a0
Ärtem-Vorobiovzquitquit()'zz <Artem-Vorobiov>
2020-04-04 15:59:38 +0800

    second

--- test.py
+++ test.py
@@ -10,7 +10,23 @@ colors = ['darkorange', 'black', 'red', 'white', 'purple']
 # for i in range(10):
 # 	print(type(random.randint(-280,280)))
 
-if type(colors) == list:
-	print('YEAH')
-else:
-	print('nope')
+# if type(colors) == list:
+# 	print('YEAH')
+# else:
+# 	print('nope')
+
+somelist = [1,2,3,4,5,6,7,8,9,10]
+count = 0
+print(somelist)
+
+for x in somelist[:]:
+	print('\t {}'.format(x))
+	somelist.remove(x)
+	# del somelist[count-1]
+	# count += 1
+
+# for i in range(len(somelist)+1):
+# 	print(i)
+# 	del somelist[i-1]
+
+print(somelist)
